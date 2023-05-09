733e40d72b821bc1b06c00e73b4ffcfcd8de0844
Ravi <rp97.reddy@gmail.com>
2023-05-09 16:51:54 +0530

    adding changes

--- env-prod/state.tfvars
+++
@@ -1,3 +0,0 @@
-bucket = "rterraform-a72"
-key    = "roboshop/prod/terraform.tfstate"
-region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,3 +1,7 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "rterraform-a72"
+    key    = "roboshop/dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
